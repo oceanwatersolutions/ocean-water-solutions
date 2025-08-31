c027252733af92ca47abed24b99f0cc2073f4c51
oceanwatersolutions <watersolutionocean@gmail.com>
2025-08-31 11:54:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ensures correct paths for GitHub Pages
+  base: '/ocean-water-solutions/', // ensures correct paths for GitHub Pages
 })
 
